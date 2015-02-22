1b8ffcbc7dd2acd5e1944436ccd8ccc1503667e3
Luke Barbuto <luke.barbuto@gmail.com>
2015-02-21 16:25:47 -0800

    Complete elixir/bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,20 @@
-defmodule Teenager do
-  def hey(input) do
-    cond do
+defmodule Message do
+  import String
 
+  def empty?(msg), do: strip(msg) ==  ""
+  def question?(msg), do: last(msg) == "?"
+  def shouting?(msg), do: upcase(msg) == msg and downcase(msg) != msg
+end
+
+defmodule Teenager do
+  import Message
+
+  def hey(msg) do
+    cond do
+      empty? msg -> "Fine. Be that way!"
+      question? msg -> "Sure."
+      shouting? msg -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
 end
