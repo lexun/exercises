8e44a5f9129f79fc70df989aacca9b1671e06b7d
Luke Barbuto <luke.barbuto@gmail.com>
2015-05-01 16:56:31 -0700

    Update elixir/word-count
    - Don't import string, just use the module normally
    - Remove @spec for private methods
    - Improve private method names
    - Use split with trim: true instead of regex followed by strip

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,8 +1,5 @@
 defmodule Words do
-  import String
-
   @invalid_symbols ~r/[^\p{L}\d'-]/u
-  @whitespace ~r/[\s_]+/
 
   @doc """
   Count the number of words in the sentence.
@@ -11,23 +8,18 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
-    sentence |> downcase |> words |> summary
+    String.downcase(sentence) |> words |> group_by_count
   end
 
-  @spec words(String.t) :: [String.t]
-  defp words(string) do
-    string
-      |> replace(@invalid_symbols, " ") |> strip
-      |> split(@whitespace)
+  defp words(sentence) do
+    String.split sentence, @invalid_symbols, trim: true
   end
 
-  @spec summary([String.t]) :: map
-  defp summary(word_list) do
-    Enum.reduce(word_list, %{}, &update/2)
+  defp group_by_count(words) do
+    Enum.reduce words, %{}, &increment_count/2
   end
 
-  @spec update(String.t, map) :: map
-  defp update(word, counts) do
-    Map.update(counts, word, 1, &(&1 + 1))
+  defp increment_count(word, counts) do
+    Map.update counts, word, 1, &(&1 + 1)
   end
 end
