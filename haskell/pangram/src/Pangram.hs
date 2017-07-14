dbf4094c3960095df5e6064c1b460f47232cfc02
Luke Barbuto <luke.barbuto@gmail.com>
2017-07-13 18:28:05 -1000

    Complete haskell/pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,4 +1,9 @@
 module Pangram (isPangram) where
 
+import Data.Char (toLower)
+
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text = all present ['a'..'z']
+  where
+    present :: Char -> Bool
+    present = (`elem` map toLower text)
