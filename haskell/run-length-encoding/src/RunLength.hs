7fd3b4f8a16226f5cb6959e393055dcaf034c96d
Luke Barbuto <luke.barbuto@gmail.com>
2017-07-13 18:28:05 -1000

    Complete haskell/run-length-encoding

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,21 @@
 module RunLength (decode, encode) where
 
+import Data.Char (isDigit)
+
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
+decode "" = ""
+decode xs = replicate n c ++ decode rest
+  where
+    c = head $ filter (not . isDigit) xs
+    d = takeWhile isDigit xs
+    n = if null d then 1 else read d
+    rest = drop (length d + 1) xs
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode "" = ""
+encode xs = d ++ c : encode rest
+  where
+    c = head xs
+    d = if n == 1 then "" else show n
+    n = length $ takeWhile (== c) xs
+    rest = drop n xs
