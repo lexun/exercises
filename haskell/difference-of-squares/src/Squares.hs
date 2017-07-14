60031ef9da9905154207017e71375a05534820a7
Luke Barbuto <luke.barbuto@gmail.com>
2017-07-13 18:28:05 -1000

    Complete haskell/difference-of-squares

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -1,10 +1,13 @@
 module Squares (difference, squareOfSums, sumOfSquares) where
 
 difference :: Integral a => a -> a
-difference n = error "You need to implement this function."
+difference n = squareOfSums n - sumOfSquares n
 
 squareOfSums :: Integral a => a -> a
-squareOfSums n = error "You need to implement this function."
+squareOfSums n = square $ sum [0..n]
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = error "You need to implement this function."
+sumOfSquares n = sum $ map square [0..n]
+
+square :: Integral a => a -> a
+square n = n * n
