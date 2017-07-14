dac882d29c9e157d65cc6449e061ad400aa8698f
Luke Barbuto <luke.barbuto@gmail.com>
2017-07-13 18:28:05 -1000

    Complete haskell/sum-of-multiples

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,7 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = sum multiples
+  where
+    multiples = filter isMultiple [0..limit - 1]
+    isMultiple n = any ((==) 0 . rem n) factors
