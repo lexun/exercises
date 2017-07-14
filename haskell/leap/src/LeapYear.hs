dfcbadd2d4c2a69a3e861bd5498ee4bd3469f9ae
Luke Barbuto <luke.barbuto@gmail.com>
2017-07-13 18:28:05 -1000

    Complete haskell/leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,4 +1,10 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = error "You need to implement this function."
+isLeapYear year
+    | isDivisibleBy 400 = True
+    | isDivisibleBy 100 = False
+    | isDivisibleBy 4   = True
+    | otherwise         = False
+  where
+    isDivisibleBy = (==) 0 . rem year
